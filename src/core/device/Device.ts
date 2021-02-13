91dc00404dd4774164df90a8b35cff8e54f3ff7c
victorfernandesraton <vfbraton@gmail.com>
2021-02-13 06:34:05 -0300

    basic device

---
+++ src/core/device/Device.ts
@@ -0,0 +1,47 @@
+enum DeviceType {
+  AVR,
+  RBPY,
+  OTHER,
+}
+
+interface IDevice {
+  macAdress: string,
+  id?: number |string,
+  name: string,
+  type?: DeviceType
+}
+
+
+export default class Device {
+  private macAddress: string;
+  private name:string;
+  private type: DeviceType;
+  constructor(args: IDevice) {
+    this.name = args.name;
+    this.macAddress = args.macAdress;
+    this.type = args.type || DeviceType.OTHER
+  }
+  public getMacAddress(): string {
+      return this.macAddress;
+  }
+
+  public setMacAddress(macAddress: string): void {
+      this.macAddress = macAddress;
+  }
+
+  public getName(): string {
+      return this.name;
+  }
+
+  public setName(name: string): void {
+      this.name = name;
+  }
+
+  public getType(): DeviceType {
+      return this.type;
+  }
+
+  public setType(type: DeviceType): void {
+      this.type = type;
+  }
+}
