57cc566add6b48c32b5834b6a80832f6ee68dda0
victorfernandesraton <vfbraton@gmail.com>
2021-02-13 07:49:19 -0300

    fix index

--- src/core/shared/either/index.ts
+++ src/core/shared/either/index.ts
@@ -1,6 +1,6 @@
 import Either from './Either';
-import * as left from './Left';
-import * as right from './Right';
+import left from './Left';
+import right from './Right';
 export const Left = left;
 export const Right = right;
 export default Either;
